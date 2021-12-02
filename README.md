a5f62c4756dd3e70eb86ef38fa609dfe6ece876e
abobakersaeed12 <abobakersaeed12@gmail.com>
2021-12-02 17:52:22 -0600

    .

---
+++ README.md
@@ -0,0 +1,12 @@
+# Abobaker
+
+**AS A** youtuber 
+**I WANT** funny gifs
+**SO THAT** I can make hilarious videos.
+
+**AS A** typer
+**I WANT TO** use gifs 
+**SO THAT** i show facial expressions 
+
+to check out the website use this Link
+https://abobakersaeed12.github.io/giphy/index.html
--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 let input = document.querySelector('[name=name]');
 let form = document.querySelector(".js-search-form");
 form.addEventListener('submit', getGifs);
+
 function getGifs(e) {
     e.preventDefault()
     let searchValue = input.value;
     input.value = '';
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=qXe0K6tr5cahunGnhKEd2278R9lwDkeE&q=${searchValue}&limit=18&offset=0&rating=g&lang=en`;
-    fetch(url).then(res => res.json())
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=qXe0K6tr5cahunGnhKEd2278R9lwDkeE&q=${searchValue}&limit=15&offset=0&rating=g&lang=en`;
+    fetch(url)
+    .then(res => res.json())
     .then(renderGifs)
-    .catch(error => {
-        console.log("error")
+    .catch(err => {
+        console.log("something is wrong")
     })
 }
+
 function renderGifs(response) {
     let gifArray = response.data;
      htmlPage = "";
