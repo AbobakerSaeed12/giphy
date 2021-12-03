ca09e365691ee2ff3aa377e064b6bbfe3b96d4f7
abobakersaeed12 <abobakersaeed12@gmail.com>
2021-12-02 23:21:50 -0600

    Indentation

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ function getGifs(e) {
     input.value = '';
     const url = `https://api.giphy.com/v1/gifs/search?api_key=qXe0K6tr5cahunGnhKEd2278R9lwDkeE&q=${searchValue}&limit=15&offset=0&rating=g&lang=en`;
     fetch(url)
-    .then(res => res.json())
-    .then(renderGifs)
-    .catch(err => {
-        console.log("something is wrong")
-    })
+        .then(res => res.json())
+        .then(renderGifs)
+        .catch(err => {
+            console.log("something is wrong");
+        });
 }
 
 function renderGifs(response) {
@@ -21,7 +21,7 @@ function renderGifs(response) {
      for (gif of gifArray){
      htmlPage += `
      <div>
-     <img class="images" src="${gif.images.original.url}" alt="${gif.title}">
+        <img class="images" src="${gif.images.original.url}" alt="${gif.title}">
      </div>
      `
      }
